17fa302703d3144864ec7f788582485ca900edc2
Doğan Can Selek <canselek2434@gmail.com>
2021-04-26 00:34:13 +0300

    addTodo fonksiyonu eklendi

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header'
-import TodoItem from './components/todoitem'
+import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo'
 
 export default function App() {
 
@@ -18,13 +19,22 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })     
+  }
+
 
   return (
     <View style={styles.container}>
       {/* Header */}
       <Header/>
       <View style={styles.content}>
-        {/*todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
